c65be8330427917035e9f956a3fd3bb30509524a
magnusmoan <magnus.moan@gmail.com>
2016-10-15 23:57:48 +0200

    .

--- Project 2/Python/agent.py
+++ Project 2/Python/agent.py
@@ -30,16 +30,49 @@ class Agent(object):
 
         print "Average reward: ", float(total_reward) / float(ep_count) 
 
+    def run_env_with_learning(self, ep_len, ep_count, debug=False):
+        env = self.env
+        total_reward = 0
+        reward_list = []
+        #env.monitor.start('/tmp/FrozenLake_test0', force=True)
+
+        for episode in xrange(ep_count):
+            print episode
+            position = env.reset()
+            #env.render()
+            for _ in xrange(ep_len):
+                action = self.select_action(position, debug)
+                new_position, reward, done, info = env.step(action)
+                self.update_q(position, action, new_position, reward)
+                #env.render()
+                position = new_position
+                total_reward += reward
+
+                if done:
+                    break
+            
+            reward_per_episode = float(total_reward) / float(ep_count)
+            reward_list.append(reward_per_episode)
+
+        #env.monitor.close()
+        #gym.upload('/tmp/FrozenLake_test0', api_key='sk_FqhrkckbSkKZ5ScFyoZ7JQ')
+        return self.q, reward_list
+
+
 class Lake_Agent(Agent):
-    def __init__(self, q=None, learning_rate=0, eps=0, discount=0):
+    def __init__(self, q=None, learning_rate=0, eps=0, discount_rate=0):
         self.env = gym.make("FrozenLake-v0")
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
 
+        self.learning_rate = learning_rate
+        self.eps = eps
+        self.discount_rate = discount_rate
+
         if q == None:
             self.q = [{'s': 0, 'w': 0, 'e': 0, 'n': 0} for _ in xrange(16)]
         else:
-            self.q = [q for _ in xrange(16)]
+            self.q = q
 
         self.action_dict = {'s': 1, 'w': 0, 'e': 2, 'n': 3}
 
@@ -60,10 +93,32 @@ class Lake_Agent(Agent):
         if p_rand <= p_north: return self.action_dict['n']
 
     
+    def update_q(self, old_position, action, new_position, reward):
+        curr_q_dict = self.q[old_position]
+        action_dir = (list((self.action_dict).keys())[list((self.action_dict).values()).index(action)])
+        neighbor_dict = self.q[new_position]
+        best_neighbor_q = max(neighbor_dict.itervalues())
+
+        added_value = self.learning_rate * (reward + self.discount_rate * best_neighbor_q - curr_q_dict[action_dir])
+        curr_q_dict[action_dir] += added_value
+
+
 class Greedy_Lake_Agent(Lake_Agent):
-    def select_action(self, position):
+    def select_action(self, position, debug=False):
         direction = max((self.q[position]).iteritems(), key=operator.itemgetter(1))[0]
-        return self.action_dict[direction]
+        p_rand = random()
+        if p_rand < self.eps:
+            action = self.action_space.sample()
+        else:
+            action = self.action_dict[direction]
+        """
+        if debug:
+            print self.q[position]
+            print direction
+            print action
+            raw_input()
+        """
+        return action
 
 class Random_Agent(Agent):
     def select_action(self, position):
--- Project 2/Python/openAI.py
+++ Project 2/Python/openAI.py
@@ -1,18 +1,46 @@
 from agent import *
+import matplotlib.pyplot as plt
+
+### Parameters ###
+episode_count = 15000
+episode_length = 100
+q = [{'s': 1.0, 'w': .5, 'n': .5, 'e': .5} for _ in xrange(16)]
+eps = .1
+learning_rate = .0915
+discount_rate = .99
 
 ### Problem 1 ###
-random_lake_agent = Random_Agent("FrozenLake-v0")
-random_taxi_agent = Random_Agent("Taxi-v1")
-#random_lake_agent.run_env(15,10)
-#random_taxi_agent.run_env(15,10)
+#random_lake_agent = Random_Agent("FrozenLake-v0")
+#random_taxi_agent = Random_Agent("Taxi-v1")
+#random_lake_agent.run_env(episode_length, episode_count)
+#random_taxi_agent.run_env(episode_length, episode_count)
 
 
 ### Problem 2a ###
-lake_agent = Lake_Agent({'s': 1.0, 'w': .5, 'n': .5, 'e': .5})
-#lake_agent.run_env(20, 10)
+#lake_agent = Lake_Agent(q=q)
+#lake_agent.run_env(episode_length, episode_count)
 
 
 ### Problem 2b ###
-greedy_lake_agent = Greedy_Lake_Agent({'s': 1.0, 'w': .5, 'n': .5, 'e': .5})
-greedy_lake_agent.run_env(20,100)
+#greedy_lake_agent = Greedy_Lake_Agent(q=q)
+#greedy_lake_agent.run_env(episode_length, episode_count)
 
+
+### Problem 2c ###
+#greedy_lake_agent = Greedy_Lake_Agent(q=q, eps=eps)
+#greedy_lake_agent.run_env(episode_length, episode_count)
+
+
+### Problem 3 ###
+lake_agent = Greedy_Lake_Agent(learning_rate=learning_rate, eps=eps, discount_rate=discount_rate)
+q, rewards = lake_agent.run_env_with_learning(episode_length, episode_count)
+print q
+print rewards[-1]
+"""
+improved_agent = Greedy_Lake_Agent(q=q, learning_rate=learning_rate, eps=0, discount_rate=discount_rate)
+_, rewards = improved_agent.run_env_with_learning(1000, 100, True)
+print rewards[-1]
+plt.plot([i for i in range(1, 100+1)], rewards)
+plt.axis([0, 100, 0, 1])
+plt.show()
+"""
