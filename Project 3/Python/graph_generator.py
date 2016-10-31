280bbe82512ccad98b5ac1f5918376ccdbb108d8
magnusmoan <magnus.moan@gmail.com>
2016-10-31 22:00:22 +0100

    Input handler changes

--- Project 3/Python/graph_generator.py
+++ Project 3/Python/graph_generator.py
@@ -5,12 +5,11 @@ def plot_graph(city_nodes, neurons, country, iterationNo=0):
 
     plt.figure().canvas.set_window_title("IT3105: Project 3")
     # Plots all cities
-    plt.plot([city_nodes[-1][0], city_nodes[0][0]], [city_nodes[-1][1], city_nodes[0][1]], color='#00ABFF')
-    city_line = plt.plot(*zip(*city_nodes), c='#00ABFF', marker='o', mew=0.0, label='Cities', ms=10)
+    city_line = plt.scatter(*zip(*city_nodes), c='#00ABFF', label='Cities')
 
     # Plots all neurons
     plt.plot([neurons[-1][0], neurons[0][0]], [neurons[-1][1], neurons[0][1]], color='#FF5500')
-    neurons_line = plt.plot(*zip(*neurons), c='#FF5500', marker='o', mew=0.0, label='Neurons', ms=10)
+    neurons_line = plt.plot(*zip(*neurons), c='#FF5500', marker='o', mew=0.0, label='Neurons', ms=5)
 
     plt.axis([-0.1,1.1,-0.1,1.1])
     plt.legend(bbox_to_anchor=(1.1, 1.1))
